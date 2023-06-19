a1771a08a431a2c4fbe14c48868ae289d80cd518
Ashish Kumawat <ashishskyblue@gmail.com>
2023-06-19 21:02:17 +0530

    adjusted shadow color

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,9 @@ export default {
     extend: {
       boxShadow: {
         // neumorphic: "0px 35px 90px 14px rgba(0,0,0,0.1)",
-        neumorphic: "0px 0px 50px 0px rgba(0,0,0,0.1)",
+        // neumorphic: "0px 0px 50px 0px rgba(0,0,0,0.1)",
+        neumorphic: "0px 10px 50px 30px #f2f3f4",
+        // neumorphic: "0px 10px 67px 21px ;",
       },
       colors: {
         "warm-gray-400": "rgb(176,176,176)",
