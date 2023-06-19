b1d679869755d54c9b47a379dba70d4c80946101
Ashish Kumawat <ashishskyblue@gmail.com>
2023-06-19 21:35:59 +0530

    adjusted shadow color to reddish

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,8 +6,10 @@ export default {
     extend: {
       boxShadow: {
         // neumorphic: "0px 35px 90px 14px rgba(0,0,0,0.1)",
-        // neumorphic: "0px 0px 50px 0px rgba(0,0,0,0.1)",
-        neumorphic: "0px 10px 50px 30px #f2f3f4",
+        // neumorphic: "0px 0px 50px 0px rgba(0,0,0,0.1)", // original
+        // neumorphic: "0px 0px 50px 0px rgba(241,211,173,0.5)", // stone color
+        neumorphic: "0px 0px 50px 0px rgba(247,194,173,0.4)", // reddish
+        // neumorphic: "0px 10px 50px 30px #f2f3f4",
         // neumorphic: "0px 10px 67px 21px ;",
       },
       colors: {
