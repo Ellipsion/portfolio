49867ae3bdb86b0504a366fcab02a49bf9722cc5
Ashish Kumawat <ashishskyblue@gmail.com>
2023-06-19 21:46:23 +0530

    adjusted shadow color to warm

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,7 @@ export default {
         // neumorphic: "0px 35px 90px 14px rgba(0,0,0,0.1)",
         // neumorphic: "0px 0px 50px 0px rgba(0,0,0,0.1)", // original
         // neumorphic: "0px 0px 50px 0px rgba(241,211,173,0.5)", // stone color
-        neumorphic: "0px 0px 50px 10px rgba(250,235,235,1)", // reddish
+        neumorphic: "0px 0px 40px 10px rgba(250,243,235,0.7)", // reddish
         // neumorphic: "0px 10px 50px 30px #f2f3f4",
         // neumorphic: "0px 10px 67px 21px ;",
       },
